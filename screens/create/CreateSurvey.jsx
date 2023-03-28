e9530ca8e0daee37904cc6d893bcc08a54b7e7e6
Nick Ciraulo <nickopicz@gmail.com>
2023-03-28 14:50:39 -0400

    added survey creation elements like dropdown choice and add question button

--- screens/create/CreateSurvey.jsx
+++ screens/create/CreateSurvey.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { RoundedButton } from "../../components/common/Button";
 import { CustomInput } from "../../components/common/Input";
 import { View, StyleSheet } from "react-native";
 import { Colors } from "../../Constants";
 import { AntDesign } from "@expo/vector-icons";
+import DropDownPicker from "react-native-dropdown-picker";
 
 
 export const CreateScreen = ({ navigation }) => {
     const styles = StyleSheet.create({
         container: {
-            alignItems: "center"
+            alignItems: "center",
         },
         codeContainer: {
             width: "100%",
@@ -18,16 +19,41 @@ export const CreateScreen = ({ navigation }) => {
             alignItems: "center",
         },
         addButton: {
+            width: "40%",
+            borderColor: Colors.navbar,
+            borderWidth: 2
+        },
+        dropdown: {
+            width: 200,
+            alignSelf: "center",
+        },
+        addContainer: {
+            alignItems: "center",
+            justifyContent: "center",
             width: "50%",
-            alignSelf: "center"
         }
     })
 
+    const questionTypes = [
+        { label: "Multiple Choice", value: "Multiple Choice" },
+        { label: "Open Ended ", value: "Open Ended" },
+        { label: "Matching", value: "Matching" },
+    ]
+
     const [title, setTitle] = useState("");
     const [code, setCode] = useState("");
+    const [pressed, setPressed] = useState(false)
+    const [choice, setChoice] = useState(questionTypes)
+    const [choiceVal, setChoiceVal] = useState(null)
+    const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState(false);
 
+
+    useEffect(() => {
+        console.log(open)
+    })
     return (
-        <View>
+        <View style={styles.container}>
             <View style={styles.codeContainer}>
                 <CustomInput small
                     placeholder="Survey Title"
@@ -43,9 +69,38 @@ export const CreateScreen = ({ navigation }) => {
                     onChangeText={(code) => setCode(code)}
                 />
             </View>
-            <RoundedButton style={styles.addButton}>
-                <AntDesign name="plus" size={50} />
-            </RoundedButton>
+            <View style={styles.addContainer}>
+                <View style={{
+                    minHeight: open === true ? 200 : 60,
+                    marginVertical: 10,
+                    justifyContent: "flex-start"
+                }}>
+                    {pressed ?
+                        <DropDownPicker
+                            style={styles.dropdown}
+                            stickyHeader={true}
+                            dropDownDirection={"down"}
+                            open={open}
+                            setOpen={setOpen}
+                            value={choiceVal}
+                            setValue={setChoiceVal}
+                            setItems={setChoice}
+                            items={choice}
+                            placeholder="Select answer"
+                            defaultIndex={0}
+                            containerStyle={{ width: 200 }}
+                            onChangeItem={item => console.log(item.label, item.value)} /> : null}
+                </View>
+                <RoundedButton style={styles.addButton} onPress={() => {
+                    setPressed(true)
+
+                }}
+                    disabled={pressed}
+                >
+                    <AntDesign name="plus" size={50} />
+
+                </RoundedButton>
+            </View>
         </View>
     )
 }
